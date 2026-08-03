59348c41da00dba310dc74e4f834eddbe31e151d
Baniero <naderharrabi1@gmail.com>
2026-08-03 20:32:30 +0100

    Ajout Dockerfile pour Render

--- MonKineBlazor.Server/Program.cs
+++ MonKineBlazor.Server/Program.cs
@@ -8,6 +8,16 @@ builder.Services.AddRazorPages();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 var app = builder.Build();
 
 try
@@ -35,6 +45,7 @@ app.UseBlazorFrameworkFiles();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseCors("AllowAll");
 app.UseAuthorization();
 
 app.MapRazorPages();
