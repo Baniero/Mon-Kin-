a80848b8bd1d265577c26a130846c6991a827106
Baniero <naderharrabi1@gmail.com>
2026-08-04 22:02:04 +0100

    Ajout Dockerfile pour Render

---
+++ MonKineBlazor.Client/Services/AuthAttributes.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MonKineBlazor.Client.Services;
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+public sealed class AllowAnonymousAttribute : Attribute
+{
+}
+
+[AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+public sealed class AdminOnlyAttribute : Attribute
+{
+}
